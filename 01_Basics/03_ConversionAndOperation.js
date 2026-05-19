9ca51834a63befcfa1ec3daaf239f6ecc92c44ce
Safariking885 <ansarilionel@gmail.com>
2026-05-19 10:27:10 +0000

    Conversion

---
+++ 01_Basics/03_ConversionAndOperation.js
@@ -0,0 +1,27 @@
+// let score = "33";
+// console.log(typeof score) //This function is use to know the datatype
+
+// let score = 33;
+// console.log(typeof score) //output number
+
+// let valueInnumber = Number(score)
+// console.log(typeof valueInnumber) //output: number
+// console.log(valueInnumber) //NaN Not a number
+// "33" = 33
+//33abc = NaN
+//true = 1, false = 0
+
+// let isloggedIn = 1
+// let booleanIsloggedIn = Boolean(isloggedIn)
+// console.log(booleanIsloggedIn);
+
+// let isloggedIn = "Safwan"
+// let booleanIsloggedIn = Boolean(isloggedIn)
+// console.log(booleanIsloggedIn);
+
+let someNumber = 33
+let otherNumber = String(someNumber)
+console.log(otherNumber)
+console.log(typeof otherNumber)
+
+
