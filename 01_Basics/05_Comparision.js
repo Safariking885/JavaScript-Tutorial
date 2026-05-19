daeae47f996e2aa4c62e923ac234beca86b5a430
Safariking885 <ansarilionel@gmail.com>
2026-05-19 10:54:37 +0000

    comparision

---
+++ 01_Basics/05_Comparision.js
@@ -0,0 +1,23 @@
+console.log(2>1)
+console.log(2>=1)
+console.log(2<1)
+console.log(2<=1)
+console.log(2==1)
+console.log(2!=1)
+
+console.log("2">1)
+console.log("02">1)
+
+console.log(null>0)
+console.log(null==0)
+console.log(null>=)
+
+console.log(undefined = 0)
+console.log(undefined==0)
+console.log(undefined>=0)
+
+/***********Strict Operator or Tripple Equal to**********/
+
+//===Triple equal operator is not only check the values but also check it datatypes
+
+console.log("2"===2)
