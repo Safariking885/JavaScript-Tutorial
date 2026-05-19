3279ab9ce81491b5e45924d708ab9d4318892502
Safariking885 <ansarilionel@gmail.com>
2026-05-19 10:38:37 +0000

    Operation

---
+++ 01_Basics/04_Operations.js
@@ -0,0 +1,32 @@
+let Value = 3
+let negValue = -Value
+console.log(negValue)
+
+console.log(2+2)
+console.log(2-2)
+console.log(2*3)
+console.log(2**3)
+console.log(2/3)
+console.log(2%3)
+
+let str1 = "Hello"
+let str2 = "Safwan"
+let str3 = str1 + str2
+console.log(str3); //Hello Safwan
+
+console.log("1" + 2)
+console.log(1 + "2")
+console.log("1" + 2 + 2)
+console.log(1 + 2 + "
+    2")
+
+console.log(+true)
+console.log(+" ")
+
+let num1, num2, num3
+num1=num2=num3=2+2
+
+/*********** Prefix and Postfix Operator***********/
+letgameCounter = 100
+gameCounter++
+console.log(gameCounter)
