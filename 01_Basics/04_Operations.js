83e49306d2f67d0ed075959f8e6d6e2ebf20653d
Safariking885 <ansarilionel@gmail.com>
2026-05-19 10:55:14 +0000

    Conver

--- 01_Basics/04_Operations.js
+++ 01_Basics/04_Operations.js
@@ -27,6 +27,6 @@ let num1, num2, num3
 num1=num2=num3=2+2
 
 /*********** Prefix and Postfix Operator***********/
-letgameCounter = 100
+let gameCounter = 100
 gameCounter++
 console.log(gameCounter)
